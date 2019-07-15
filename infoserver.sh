a2864c3bb42e7778ff227137ab6487b5c181f066
Zaki Akhyar <zaki.akhyar@outlook.com>
2019-07-15 18:12:08 +0700

    Code cleaning
    Code cleaning

--- infoserver.sh
+++ infoserver.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
-# ------------------ JSON START ------------------------ #
-echo -n "{"
 
-# ------------------- TIMESTAMP ------------------------ #
+echo -n "{"
 echo -n "\"timestamp\" : "
 echo -n "\"`date -u '+%FT%T.000Z'`\", "
-
-# ------------------- HOSTNAME ------------------------- #
 echo -n "\"hostname\" : "
 echo -n "\"`hostname`\", "
-
-# --------------------- IP ----------------------------- #
 echo -n "\"ip\" : "
 echo -n "\"`hostname -I`\","
-
-# ------------------ HITUNG USER LOGIN ----------------- #
 echo -n "\"users_sedang_login\":\""
 uptime | grep -ohe '[0-9.*] user[s,]' | awk '{ print $1 "\","}'
-
-# ---------------------- UPTIME ------------------------ #
 echo -n "\"uptime\" : \""
 uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2" "$3 "\", " }'
 #echo -n "\", "
 
-# -------------------- CEK SERVICES -------------------- #
+# SERVICES #
 echo -n "\"services\": { "
 SERVICE=httpd
 if ps ax | grep -v grep | grep $SERVICE > /dev/null; then echo -n "\"$SERVICE\" : \"running\","; else echo -n "\"$SERVICE\" : \"not running\","; fi
+# Tambah service baru pisah dengan : echo -n "," 
+	# SERVICE=httpd
+	# if ps ax | grep -v grep | grep $SERVICE > /dev/null; then echo -n "\"$SERVICE\" : \"running\","; else echo -n "\"$SERVICE\" : \"not running\","; fi
+	# echo -n ","
+
 SERVICE=sshd
 if ps ax | grep -v grep | grep $SERVICE > /dev/null; then echo -n "\"$SERVICE\" : \"running\""; else echo -n "\"$SERVICE\" : \"not running\""; fi
 echo -n " }, "
+# / SERVICE #
 
-# --------------------- CEK DISK ----------------------- #
 echo -n "\"disk\" : { ";
 echo -n "\""
 df -h --total | awk  ' /total/ { print "total\" : \""$2"\", \"used\" : \""$3"\", \"free\" : \""$4"\", \"percentage\" : \""$5"\'\"' " }'
 echo "},"
 
-# --------------------- CEK MEMORY --------------------- #
 echo -n "\"memory\" : { ";
 echo -n "\"free_ram\" : \""
 free -m | grep -v shared | awk '/Mem/ {printf $4 }'
@@ -46,7 +40,6 @@ echo -n "\"total_ram\" : \""
 free -m | grep -v shared | awk '/Mem/ {printf $2 }'
 echo '",'
 
-# -------------------- JSON CLOSE ---------------------- #
 echo -n "\"json_close\" : \"close\""
 echo " }} "
 
