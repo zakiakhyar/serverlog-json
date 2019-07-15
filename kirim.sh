4b9ab5a4fb084639525d4197d658ea888d1e6c82
zakiakhyar <zaki.akhyar@hotmail.com>
2019-07-15 11:48:16 +0700

    Add service to infoserver, change send method curl to API

--- kirim.sh
+++ kirim.sh
@@ -4,7 +4,8 @@ function d_start (){
 while true 
 do
 	bash /opt/siatlog/infoserver.sh | tr '\n' ' ' > /tmp/infoserver.json;
-	curl -XPOST 'IP_Elastic:9200/server/logs/' --header 'Content-Type: application/json' -d @/tmp/infoserver.json;	
+	#curl -XPOST 'api.serverlog-json.com/api/insert/' --header 'Content-Type: application/x-www-form-urlencoded' -d "key=123&jsondata=@/tmp/infoserver.json";	
+	curl -s -o -XPOST 'api.serverlog-json.com/api/insert/' --header 'Content-Type: application/x-www-form-urlencoded' -d "key=123&jsondata=`cat /tmp/infoserver.json`"
 	sleep 30;
 done &
 }
